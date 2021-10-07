ce3cc09eb9bb5fcc4b7729cd3c66ec46180b4609
ganin.n.a <nikitaganin>
2021-10-07 11:49:16 +0300

    7.2: routed anecdotes, step2(add showing single anecdote)

---
+++ src/components/Anecdote.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Anecdote = ({ anecdote }) => {
+    console.log(anecdote)
+    return (
+        <div>
+            <h2>{anecdote.content}</h2>
+            <div>has {anecdote.votes} votes</div>
+            <br/>
+            <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div>
+            <br/>
+        </div>
+    )
+}
+
+export default Anecdote
